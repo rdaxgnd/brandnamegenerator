6e5aef79267179b644312a8e449a788592698769
Arda <116800425+rdaxgnd@users.noreply.github.com>
2023-09-08 03:00:36 +0300

    Version1

--- app/page.js
+++ app/page.js
@@ -1,95 +1,56 @@
-import Image from 'next/image'
+'use client';
+import React from 'react'
+import { useState } from 'react'
 import styles from './page.module.css'
 
+// Generator settings as basic
+const varLetters = {
+  group1: ['a', 'e', 'i', 'o', 'u'],
+  group2: ['r', 't', 'y', 'p', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm'],
+}
+
 export default function Home() {
+
+  const [groupOne, setGroupOne] = React.useState({ one: 'a' })
+  const [groupTwo, setGroupTwo] = React.useState({ two: 'r' })
+  const [groupThree, setGroupThree] = React.useState({ three: 'a' })
+  const [groupFour, setGroupFour] = React.useState({ four: 'r' })
+  const [groupFive, setGroupFive] = React.useState({ five: 'a' })
+  const [groupSix, setGroupSix] = React.useState({ six: 'r' })
+
+  function generateWord() {
+    setGroupOne({one: varLetters.group1[Math.floor(Math.random() * varLetters.group1.length)]})
+    setGroupTwo({two: varLetters.group2[Math.floor(Math.random() * varLetters.group2.length)]})
+    setGroupThree({three: varLetters.group1[Math.floor(Math.random() * varLetters.group1.length)]})
+    setGroupFour({four: varLetters.group2[Math.floor(Math.random() * varLetters.group2.length)]})
+    setGroupFive({five: varLetters.group1[Math.floor(Math.random() * varLetters.group1.length)]})
+    setGroupSix({six: varLetters.group2[Math.floor(Math.random() * varLetters.group2.length)]})
+  }
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+
+      <div className={styles.frame}>
+        
+        <div>{groupOne.one}{groupTwo.two}{groupThree.three}{groupFour.four}{groupFive.five}{groupSix.six}</div>
+        <div>{groupTwo.two}{groupFive.five}{groupOne.one}{groupFour.four}</div>
+        <div>{groupFour.four}{groupOne.one}{groupTwo.two}{groupSix.six}{groupFive.five}</div>
+        <div>{groupSix.six}{groupFive.five}{groupFour.four}{groupThree.three}{groupTwo.two}{groupOne.one}</div>
+        <div>{groupTwo.two}{groupThree.three}{groupFour.four}{groupFive.five}{groupSix.six}{groupOne.one}</div>
+        <div>{groupTwo.two}{groupFive.five}{groupSix.six}{groupOne.one}{groupFour.four}</div>
+        <div>{groupOne.one}{groupTwo.two}{groupSix.six}{groupFive.five}{groupFour.four}</div>
+        <div>{groupFour.four}{groupThree.three}{groupTwo.two}{groupOne.one}</div>
+        
+
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className={styles.bar}>
+        
+        <div style={{textAlign: 'left', paddingTop: '12px'}}>Brand Name Generator</div>
+        <div className={styles.button} onClick={generateWord}>Generate</div>
+
       </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   )
 }
